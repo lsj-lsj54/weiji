894775fa10fb05ad8147f8d44acbc427b8d18c9f
lsj-lsj54 <3520023395@qq.com>
2026-09-11 14:03:12 +0800

    微积初始骨架

---
+++ src/test/java/com/lsj/weijibackend/WeijiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsj.weijibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeijiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
